42325bebe7b05303196d6de368546b375af10021
Sasha <dezeercraft184@gmail.com>
2025-11-02 21:33:03 +0300

    Version 1.2

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 # One PythonBot
-# Версия 1.1
+# Версия 1.2
 
 import telebot, pickle, random, time
 from telebot import TeleBot
 import wisdom, jokes
 
-
+"""
 def load_token():
     with open("token.dat", "rb") as file:
         return pickle.load(file)
-    
-bot = TeleBot(load_token())
+"""
 
+TOKEN = '8207941725:AAHlCI0KzzDU9Vg0Yk-gh2UB2_plyEhg_-M'
+bot = TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-     bot.send_message(message.chat.id, text="Ку!")
+    bot.send_message(message.chat.id, text="Ку! Я удобный бот:)")
 
 @bot.message_handler(commands=["wisdom"])
 def print_wisdom(message):
@@ -26,20 +27,29 @@ def print_wisdom(message):
 def get_joke(message):
     bot.reply_to(message, random.choice(jokes.JOKES))
 
-@bot.message_handler(commands=["money"])
+@bot.message_handler(commands=["random"])
 def money(message):
     randint = random.randint(0, 100)
-    bot.reply_to(message, text="Бросаю монетку...")
+    bot.reply_to(message, text="Раскручиваю автомат...")
+
+    bot.send_message(message.chat.id, text="\U0001F3B0")
     time.sleep(2)
 
     if randint <= 50:
-        bot.reply_to(message, text="Выпал орёл.")
+        bot.reply_to(message, text="Вы выиграли.")
     elif randint > 50:
-        bot.reply_to(message, text="Выпала решка.")
+        bot.reply_to(message, text="Вы проиграли.")
 
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.reply_to(message, text="Доступные команды:  \n/start\n/wisdom\n/joke\n/money\n/help") 
+
+
+@bot.message_handler(content_types=["text"])
+def text_test(message):
+    if message.text.lower() == "тест":
+        bot.send_message(message.chat.id, "Успех!")
+
 
 
 bot.polling()
